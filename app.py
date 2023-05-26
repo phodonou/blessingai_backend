df60b7814f8db591876d746b8193a23ba3bfaa83
Prince  Hodonou <folucho@gmail.com>
2023-05-26 17:33:05 -0500

    websockets issue

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def ask(df, query, updatestep, n=3):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8080)
+    socketio.run(app, host='0.0.0.0', port=5000, keyfile='server.key', certfile='server.cert')
